8e16ba9066c6d5403faf90714b5da82825509f06
bujars <bujarsefa9@gmail.com>
2019-11-23 23:05:39 -0500

    Adding comments to Double Check code

--- Cook: onAddIngredient.js
+++ Cook: onAddIngredient.js
@@ -1,9 +1,10 @@
 
 
 onAddIngredient = async () => {
+       /* Should check if the ingredient already exists */
         const {name, quantity, specialty, specialRequest} = this.state;
         if (res.user.uid !== null || res.user.uid !== undefined) {
-            await firebase.database().ref(`/ingredients/${res.ingredient.iid}`)
+            await firebase.database().ref(`/ingredients/${res.ingredient.iid}`) /* better name for folder/ iid? */
             .set({
                 name: name,
                 quantity: quantity,
