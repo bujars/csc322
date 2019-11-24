0d5a0cad31bd8cae1c982b7a7e5becd3c2b0c54c
bujars <bujarsefa9@gmail.com>
2019-11-23 23:08:32 -0500

    Adding comments for project. See last commit about comments for code alterations

--- Cook: onAddIngredient.js
+++ Cook: onAddIngredient.js
@@ -1,10 +1,13 @@
 
 
 onAddIngredient = async () => {
-       /* Should check if the ingredient already exists */
+       //Pull in the user input (parameters)
         const {name, quantity, specialty, specialRequest} = this.state;
+       //Authenticate User ==> Just as a safety check
         if (res.user.uid !== null || res.user.uid !== undefined) {
-            await firebase.database().ref(`/ingredients/${res.ingredient.iid}`) /* better name for folder/ iid? */
+           //Connect to database: Ingreidents Table 
+            await firebase.database().ref(`/ingredients/${res.ingredient.iid}`)
+            //Create a new ingredient with following attributes
             .set({
                 name: name,
                 quantity: quantity,
@@ -12,6 +15,7 @@ onAddIngredient = async () => {
                 specialRequest: specialRequest
             }); 
         }.catch( (error) => {
+               //Handle errors
                 console.log(error)
         });
     }
